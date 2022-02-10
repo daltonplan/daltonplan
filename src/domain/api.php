d6fec8f1a740caf20c73b995166f224b99073c54
Lava Block <the@lava-block.com>
2022-02-10 09:56:31 +0100

    check /api/time

--- src/domain/api.php
+++ src/domain/api.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace domain;
 
+use frame\time;
+
 class api extends \frame\app
 {
     function version(\Base $fw): void
     {
         echo $fw->get('version');
+    }
+
+    function time(\Base $fw): void
+    {
+        echo time::get_for_db($fw);
     }
 }
